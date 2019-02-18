6d32be2596547a5b5d2afc05884e94e7664a9e84
svphan29 <36240410+svphan29@users.noreply.github.com>
2019-02-17 20:10:46 -0700

    define turn, checkWin, and gameOver functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var originalBoard;
-const humanPlayer = 'O';
-const aiPlayer = 'X';
+var newBoard;
+const humanPlayer = "O";
+const aiPlayer = "X";
 const winCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -11,3 +11,55 @@ const winCombos = [
   [0, 4, 8],
   [6, 4, 2]
 ]
+
+const cells = document.querySelectorAll(".cell");
+startGame();
+
+function startGame() {
+  document.querySelector(".endgame").style.display = "none";
+  newBoard = Array.from(Array(9).keys());
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  }
+}
+
+function turnClick(square) {
+  turn(square.target.id, humanPlayer)
+}
+
+function turn(squareId, player) {
+  newBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(newBoard, player)
+  if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, ele, i) =>
+    (ele === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for (let [index, win] of winCombos.entries()) {
+      if (win.every(elem => plays.indexOf(elem) > -1)) {
+        gameWon = {index: index, player: player};
+        break;
+      }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = gameWon.player === humanPlayer ? "blue" : "red";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false)
+  }
+}
+
+
+
+
+
+
